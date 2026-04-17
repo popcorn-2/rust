0179482a10693570b91bc7a26c00257edf0ecd62
Eliyahu Gluschove-Koppel <egkoppel@eliyahu.co.uk>
2026-04-17 13:30:31 +0100

    add very hacky envvar support to popcorn that doesn't work at all with ffi

--- library/std/src/sys/env/popcorn.rs
+++ library/std/src/sys/env/popcorn.rs
@@ -2,30 +2,41 @@ use crate::ffi::{OsStr, OsString};
 use crate::io;
 pub use super::common::Env;
 use crate::os::popcorn::ffi::OsStrExt;
+use crate::sync::{OnceLock, nonpoison::Mutex};
+use crate::collections::HashMap;
+
+fn internal_env() -> &'static Mutex<HashMap<OsString, OsString>> {
+    static ENV: OnceLock<Mutex<HashMap<OsString, OsString>>> = OnceLock::new();
+
+    ENV.get_or_init(|| {
+        let strs = crate::sys::get_env();
+        let mut map = HashMap::new();
+
+        for s in strs {
+            let s = <OsStr as OsStrExt>::as_str(s);
+            if let Some((key, val)) = s.split_once("=") {
+                map.insert(OsStr::from_str(key).to_owned(), OsStr::from_str(val).to_owned());
+            }
+        }
+
+        Mutex::new(map)
+    })
+}
 
 pub fn env() -> Env {
-    let strs = crate::sys::get_env();
-    let mut vec = vec![];
-
-    for s in strs {
-        let s = <OsStr as OsStrExt>::as_str(s);
-	    if let Some((key, val)) = s.split_once("=") {
-		    vec.push((OsStr::from_str(key).to_owned(), OsStr::from_str(val).to_owned()));
-	    }
-    }
-
-    Env::new(vec)
+    Env::new(internal_env().lock().iter().map(|(k,v)| (k.clone(), v.clone())).collect())
 }
 
-pub fn getenv(find_key: &OsStr) -> Option<OsString> {
-    // fixme: not linear search
-    env().find_map(|(key, val)| (find_key == key).then(|| val.clone()))
+pub fn getenv(key: &OsStr) -> Option<OsString> {
+    internal_env().lock().get(key).map(|val| val.clone())
 }
 
-pub unsafe fn setenv(_: &OsStr, _: &OsStr) -> io::Result<()> {
-    Err(io::const_error!(io::ErrorKind::Unsupported, "cannot set env vars on this platform"))
+pub unsafe fn setenv(key: &OsStr, val: &OsStr) -> io::Result<()> {
+    internal_env().lock().insert(key.to_owned(), val.to_owned());
+    Ok(())
 }
 
-pub unsafe fn unsetenv(_: &OsStr) -> io::Result<()> {
-    Err(io::const_error!(io::ErrorKind::Unsupported, "cannot unset env vars on this platform"))
+pub unsafe fn unsetenv(key: &OsStr) -> io::Result<()> {
+    internal_env().lock().remove(key);
+    Ok(())
 }
