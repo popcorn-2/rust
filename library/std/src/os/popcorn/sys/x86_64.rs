7530dee412de84dc1e7c403be332e47aadbff33d
Eliyahu Gluschove-Koppel <egkoppel@eliyahu.co.uk>
2026-05-24 21:27:12 +0100

    add async syscall macro

--- library/std/src/os/popcorn/sys/x86_64.rs
+++ library/std/src/os/popcorn/sys/x86_64.rs
@@ -134,5 +134,150 @@ pub macro syscall {
             );
             match (high as u128) << 64 | (low as u128) { res => ::core::result::Result::Ok(res), }
         }
-    }
+    },
+    (async($key:expr) $uid:expr, $arg0:expr) => {
+        'syscall: {
+            let uid = <u128 as ::core::convert::From<_>>::from($uid);
+            let arg0: $crate::os::popcorn::io::RawHandle = $arg0;
+            let key = <usize as ::core::convert::From<_>>::from($key);
+            let low: u64;
+            let high: u64;
+            ::core::arch::asm!(
+                "stc",
+                "syscall",
+                "jc {error}",
+                inout("rax") uid as u64 => low,
+                out("rcx") _,
+                out("rdx") high,
+                out("rsi") _,
+                inout("rdi") arg0.0 => _,
+                out("r8") _,
+                inout("r9") (uid >> 64) as u64 => _,
+                out("r10") _,
+                out("r11") _,
+                out("r12") _,
+                in("r15") key,
+                error = label { match (high as u128) << 64 | (low as u128) { err => break 'syscall ::core::result::Result::Err(err), } }
+            );
+            match (high as u128) << 64 | (low as u128) { res => ::core::result::Result::Ok(res), }
+        }
+    },
+    (async($key:expr) $uid:expr, $arg0:expr, $arg1:expr) => {
+        'syscall: {
+            let uid = <u128 as ::core::convert::From<_>>::from($uid);
+            let arg0: $crate::os::popcorn::io::RawHandle = $arg0;
+            let arg1 = <usize as ::core::convert::From<_>>::from($arg1);
+            let key = <usize as ::core::convert::From<_>>::from($key);
+            let low: u64;
+            let high: u64;
+            ::core::arch::asm!(
+                "clc",
+                "syscall",
+                "jc {error}",
+                inout("rax") uid as u64 => low,
+                out("rcx") _,
+                out("rdx") high,
+                inout("rdi") arg0.0 => _,
+                inout("rsi") arg1 => _,
+                out("r8") _,
+                inout("r9") (uid >> 64) as u64 => _,
+                out("r10") _,
+                out("r11") _,
+                out("r12") _,
+                in("r15") key,
+                error = label { match (high as u128) << 64 | (low as u128) { err => break 'syscall ::core::result::Result::Err(err), } }
+            );
+            match (high as u128) << 64 | (low as u128) { res => ::core::result::Result::Ok(res), }
+        }
+    },
+    (async($key:expr) $uid:expr, $arg0:expr, $arg1:expr, $arg2:expr) => {
+        'syscall: {
+            let uid = <u128 as ::core::convert::From<_>>::from($uid);
+            let arg0: $crate::os::popcorn::io::RawHandle = $arg0;
+            let arg1 = <usize as ::core::convert::From<_>>::from($arg1);
+            let arg2 = <usize as ::core::convert::From<_>>::from($arg2);
+            let key = <usize as ::core::convert::From<_>>::from($key);
+            let low: u64;
+            let high: u64;
+            ::core::arch::asm!(
+                "clc",
+                "syscall",
+                "jc {error}",
+                inout("rax") uid as u64 => low,
+                out("rcx") _,
+                inout("rdx") arg2 => high,
+                inout("rdi") arg0.0 => _,
+                inout("rsi") arg1 => _,
+                out("r8") _,
+                inout("r9") (uid >> 64) as u64 => _,
+                out("r10") _,
+                out("r11") _,
+                out("r12") _,
+                in("r15") key,
+                error = label { match (high as u128) << 64 | (low as u128) { err => break 'syscall ::core::result::Result::Err(err), } }
+            );
+            match (high as u128) << 64 | (low as u128) { res => ::core::result::Result::Ok(res), }
+        }
+    },
+    (async($key:expr) $uid:expr, $arg0:expr, $arg1:expr, $arg2:expr, $arg3: expr) => {
+        'syscall: {
+            let uid = <u128 as ::core::convert::From<_>>::from($uid);
+            let arg0: $crate::os::popcorn::io::RawHandle = $arg0;
+            let arg1 = <usize as ::core::convert::From<_>>::from($arg1);
+            let arg2 = <usize as ::core::convert::From<_>>::from($arg2);
+            let arg3 = <usize as ::core::convert::From<_>>::from($arg3);
+            let key = <usize as ::core::convert::From<_>>::from($key);
+            let low: u64;
+            let high: u64;
+            ::core::arch::asm!(
+                "clc",
+                "syscall",
+                "jc {error}",
+                inout("rax") uid as u64 => low,
+                out("rcx") _,
+                inout("rdx") arg2 => high,
+                inout("rdi") arg0.0 => _,
+                inout("rsi") arg1 => _,
+                out("r8") _,
+                inout("r9") (uid >> 64) as u64 => _,
+                inout("r10") arg3 => _,
+                out("r11") _,
+                out("r12") _,
+                in("r15") key,
+                error = label { match (high as u128) << 64 | (low as u128) { err => break 'syscall ::core::result::Result::Err(err), } }
+            );
+            match (high as u128) << 64 | (low as u128) { res => ::core::result::Result::Ok(res), }
+        }
+    },
+    (async($key:expr) $uid:expr, $arg0:expr, $arg1:expr, $arg2:expr, $arg3:expr, $arg4:expr) => {
+        'syscall: {
+            let uid = <u128 as ::core::convert::From<_>>::from($uid);
+            let arg0: $crate::os::popcorn::io::RawHandle = $arg0;
+            let arg1 = <usize as ::core::convert::From<_>>::from($arg1);
+            let arg2 = <usize as ::core::convert::From<_>>::from($arg2);
+            let arg3 = <usize as ::core::convert::From<_>>::from($arg3);
+            let arg4 = <usize as ::core::convert::From<_>>::from($arg4);
+            let key = <usize as ::core::convert::From<_>>::from($key);
+            let low: u64;
+            let high: u64;
+            ::core::arch::asm!(
+                "clc",
+                "syscall",
+                "jc {error}",
+                inout("rax") uid as u64 => low,
+                out("rcx") _,
+                inout("rdx") arg2 => high,
+                inout("rdi") arg0.0 => _,
+                inout("rsi") arg1 => _,
+                inout("r8") arg4 => _,
+                inout("r9") (uid >> 64) as u64 => _,
+                inout("r10") arg3 => _,
+                out("r11") _,
+                out("r12") _,
+                in("r15") key,
+                error = label { match (high as u128) << 64 | (low as u128) { err => break 'syscall ::core::result::Result::Err(err), } }
+            );
+            match (high as u128) << 64 | (low as u128) { res => ::core::result::Result::Ok(res), }
+        }
+    },
 }
