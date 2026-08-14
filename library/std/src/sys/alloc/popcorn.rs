4f4b8c769cc6d86ee3c6c2024e4612d9e3677c5a
Eliyahu Gluschove-Koppel <egkoppel@eliyahu.co.uk>
2026-08-14 16:34:04 +0100

    replace popcorn libc allocator with dlmalloc

--- library/std/src/sys/alloc/popcorn.rs
+++ library/std/src/sys/alloc/popcorn.rs
@@ -1,55 +1,98 @@
-use crate::alloc::{GlobalAlloc, Layout, System};
-use super::{MIN_ALIGN, realloc_fallback};
-use core::ffi::c_void;
-use crate::ptr;
+use core::cell::SyncUnsafeCell;
 
-// todo(popcorn): remove dependency on libc - we don't need to worry about cross-lang malloc/free cause that's UB anyway
+use crate::alloc::Layout;
 
-#[cfg_attr(target_feature = "crt-static", link(name = "libc.a", modifiers="+verbatim"))]
-#[cfg_attr(not(target_feature = "crt-static"), link(name = "libc.so", modifiers="+verbatim"))]
-unsafe extern "C" {
-    fn aligned_alloc(__alignment: usize, __size: usize) -> *mut c_void;
-    fn calloc(__count: usize, __size: usize) -> *mut c_void;
-    fn free(__pointer: *mut c_void);
-    fn malloc(__size: usize) -> *mut c_void;
-    fn realloc(__pointer: *mut c_void, __size: usize) -> *mut c_void;
+struct SyncDlmalloc(dlmalloc::Dlmalloc);
+unsafe impl Sync for SyncDlmalloc {}
+
+static mut DLMALLOC: dlmalloc::Dlmalloc<Vexos> = dlmalloc::Dlmalloc::new_with_allocator(Vexos);
+
+struct Popcorn;
+
+unsafe impl dlmalloc::Allocator for Popcorn {
+    fn alloc(&self, _size: usize) -> (*mut u8, usize, u32) {
+        todo!()
+    }
+
+    fn remap(&self, _ptr: *mut u8, _oldsize: usize, _newsize: usize, _can_move: bool) -> *mut u8 {
+        todo!()
+    }
+
+    fn free_part(&self, _ptr: *mut u8, _oldsize: usize, _newsize: usize) -> bool {
+        false
+    }
+
+    fn free(&self, _ptr: *mut u8, _size: usize) -> bool {
+        todo!()
+    }
+
+    fn can_release_part(&self, _flags: u32) -> bool {
+        false
+    }
+
+    fn allocates_zeros(&self) -> bool {
+        false
+    }
+
+    fn page_size(&self) -> usize {
+        4096
+    }
 }
 
-#[stable(feature = "alloc_system_type", since = "1.28.0")]
-unsafe impl GlobalAlloc for System {
-    #[inline]
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        if layout.align() <= MIN_ALIGN {
-            unsafe { malloc(layout.size()) as *mut u8 }
-        } else {
-            unsafe { aligned_alloc(layout.align(), layout.size()) as *mut u8 }
-        }
-    }
+#[inline]
+pub unsafe fn alloc(layout: Layout) -> *mut u8 {
+    // SAFETY: DLMALLOC access is guaranteed to be safe because the lock gives us unique and non-reentrant access.
+    // Calling malloc() is safe because preconditions on this function match the trait method preconditions.
+    let _lock = lock::lock();
+    unsafe { DLMALLOC.malloc(layout.size(), layout.align()) }
+}
 
-    #[inline]
-    unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
-        if layout.align() <= MIN_ALIGN {
-            unsafe { calloc(layout.size(), 1) as *mut u8 }
-        } else {
-            let ptr = unsafe { self.alloc(layout) };
-            if !ptr.is_null() {
-                unsafe { ptr::write_bytes(ptr, 0, layout.size()) };
+#[inline]
+pub unsafe fn alloc_zeroed(layout: Layout) -> *mut u8 {
+    // SAFETY: DLMALLOC access is guaranteed to be safe because the lock gives us unique and non-reentrant access.
+    // Calling calloc() is safe because preconditions on this function match the trait method preconditions.
+    let _lock = lock::lock();
+    unsafe { DLMALLOC.calloc(layout.size(), layout.align()) }
+}
+
+#[inline]
+pub unsafe fn dealloc(ptr: *mut u8, layout: Layout) {
+    // SAFETY: DLMALLOC access is guaranteed to be safe because the lock gives us unique and non-reentrant access.
+    // Calling free() is safe because preconditions on this function match the trait method preconditions.
+    let _lock = lock::lock();
+    unsafe { DLMALLOC.free(ptr, layout.size(), layout.align()) }
+}
+
+#[inline]
+pub unsafe fn realloc(ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
+    // SAFETY: DLMALLOC access is guaranteed to be safe because the lock gives us unique and non-reentrant access.
+    // Calling realloc() is safe because preconditions on this function match the trait method preconditions.
+    let _lock = lock::lock();
+    unsafe { DLMALLOC.realloc(ptr, layout.size(), layout.align(), new_size) }
+}
+
+// FIXME: use proper mutex
+mod lock {
+    use crate::sync::atomic::Ordering::{Acquire, Release};
+    use crate::sync::atomic::{Atomic, AtomicI32};
+
+    static LOCKED: Atomic<i32> = AtomicI32::new(0);
+
+    pub struct DropLock;
+
+    pub fn lock() -> DropLock {
+        loop {
+            if LOCKED.swap(1, Acquire) == 0 {
+                return DropLock;
             }
-            ptr
+            crate::thread::yield_now();
         }
     }
 
-    #[inline]
-    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-        unsafe { free(ptr as *mut c_void) }
-    }
-
-    #[inline]
-    unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
-        if layout.align() <= MIN_ALIGN {
-            unsafe { realloc(ptr as *mut c_void, new_size) as *mut u8 }
-        } else {
-            unsafe { realloc_fallback(self, ptr, layout, new_size) }
+    impl Drop for DropLock {
+        fn drop(&mut self) {
+            let r = LOCKED.swap(0, Release);
+            debug_assert_eq!(r, 1);
         }
     }
 }
